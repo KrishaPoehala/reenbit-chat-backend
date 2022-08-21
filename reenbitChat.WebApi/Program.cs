b32610e720491c739e775a322e6339de8beb8335
unknown <kovalikn6@gmail.com>
2022-08-21 14:02:32 +0300

    added auto-migraiton when runing the app for the fist time

--- reenbitChat.WebApi/Program.cs
+++ reenbitChat.WebApi/Program.cs
@@ -7,6 +7,7 @@ using reenbitChat.DAL.Context;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+var migrationAssebly = typeof(ChatContext).Assembly.GetName().Name;
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
     builder.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
@@ -14,7 +15,8 @@ builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 }));
 // Add services to the container.
 builder.Services.AddDbContext<ChatContext>(x => 
-x.UseSqlServer(builder.Configuration.GetConnectionString("ChatDb")));
+x.UseSqlServer(builder.Configuration.GetConnectionString("ChatDb"), 
+opt => opt.MigrationsAssembly(migrationAssebly)));
 builder.Services.AddAutoMapper(typeof(reenbitChat.Common.Profiles.MapperProfile));
 builder.Services.AddControllers().AddJsonOptions(x =>
                 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
@@ -40,4 +42,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
+InitializeDb(app);
+
+
 app.Run();
+
+void InitializeDb(IApplicationBuilder app)
+{
+    using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+    {
+        using var context = scope.ServiceProvider.GetRequiredService<ChatContext>();
+        context.Database.Migrate();
+    };
+}
